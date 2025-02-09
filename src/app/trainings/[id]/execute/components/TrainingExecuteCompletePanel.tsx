a09a59ba96cc03813e1f86e8e53298c6086dbcc4
Nikita Dezzpil Orlov <n.dezz.orlov@gmail.com>
2025-02-09 16:42:02 +0300

    feat: добавить анимацию конфетти при завершении тренировки

--- src/app/trainings/[id]/execute/components/TrainingExecuteCompletePanel.tsx
+++ src/app/trainings/[id]/execute/components/TrainingExecuteCompletePanel.tsx
@@ -1,12 +1,27 @@
 "use client";
 import type { Training } from "@prisma/client";
-import React, { useCallback, useState } from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import moment from "moment";
 import { TrainingRepeatForm } from "@/app/trainings/[id]/execute/components/TrainingRepeatForm";
 import { TrainingProcessPanel } from "@/app/trainings/[id]/execute/components/TrainingProcessPanel";
 import { handleCompleteTrainingManually } from "@/app/trainings/[id]/execute/actions";
 import Link from "next/link";
 import { TimeFormat } from "@/tools/dates";
+import Confetti from "react-dom-confetti";
+import {ConfettiConfig} from "dom-confetti";
+
+const ConfettiConfig1: ConfettiConfig = {
+  elementCount: 75,
+  duration: 3000,
+  angle: 45,
+  spread: 45
+};
+const ConfettiConfig2: ConfettiConfig = {
+  elementCount: 75,
+  duration: 3000,
+  angle: 135,
+  spread: 45
+};
 
 type Props = {
   training: Training;
@@ -14,34 +29,44 @@ type Props = {
 export function TrainingExecuteCompletePanel({ training }: Props) {
   const [error, setError] = useState<null | string>(null);
   const [handling, setHandling] = useState<boolean>(false);
+  const [isConfettiActive, setConfettiActive] = useState(false);
+  useEffect(() => {
+    if (training.completedAt) {
+      setConfettiActive(true);
+      setTimeout(() => {
+        setConfettiActive(false);
+      }, 3000);
+    }
+  }, [training.completedAt]);
   const handle = useCallback(async () => {
     setHandling(true);
     setError(null);
     try {
-      await handleCompleteTrainingManually(training.id);
-    } catch (e: any) {
-      console.error(e);
-      setError(e.message);
-    } finally {
-      setHandling(false);
-    }
+        await handleCompleteTrainingManually(training.id);
+      } catch (e: any) {
+        console.error(e);
+        setError(e.message);
+      } finally {
+        setHandling(false);
+      }
   }, [training.id]);
   return (
-    <div className="alert alert-light d-flex gap-5 justify-content-between align-items-top">
+    <div>
+    <div className="alert alert-light d-flex justify-content-between">
+      <Confetti active={ isConfettiActive } config={ConfettiConfig1} />
+      <div className='flex-fill d-flex gap-3 justify-content-end align-items-center'>
       {training.completedAt ? (
         <>
-          <div className="">
-            <span>
-              Завершена в {moment(training.completedAt).format(TimeFormat)} (+
-              {moment(training.completedAt).diff(
-                moment(training.startedAt),
-                "minute",
-              )}{" "}
-              мин.)!
-            </span>
-            <Link href={`/trainings/${training.id}`}>Подробнее...</Link>
-          </div>
-
+          <span>
+            Завершена в {moment(training.completedAt).format(TimeFormat)} (+
+            {moment(training.completedAt).diff(
+              moment(training.startedAt),
+              "minute",
+            )}{" "}
+            мин.)
+          </span>
+          <Link className={'btn btn-outline-primary'} href={`/trainings/${training.id}`}>Подробнее...</Link>
+          <button className="btn btn-default" onClick={() => { setConfettiActive(true); }}>Ура!</button>
           {training.processedAt ? (
             <TrainingRepeatForm training={training} />
           ) : (
@@ -68,6 +93,9 @@ export function TrainingExecuteCompletePanel({ training }: Props) {
           {error && <div className="alert alert-danger">{error}</div>}
         </>
       )}
+      </div>
+      <Confetti active={ isConfettiActive } config={ConfettiConfig2} />
+    </div>
     </div>
   );
 }
