52661cd1851af7e9079aa735c0d2cf3be7a3f8d8
Nikita Dezzpil Orlov <n.dezz.orlov@gmail.com>
2025-11-30 18:18:01 +0300

    feat: отказаться от гугл.фонтс

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
 import React from "react";
 import BootstrapJS from "@/components/BootstrapJS";
 import AuthProvider from "@/components/AuthProvider";
 import { Layout } from "@/components/Layout";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const dynamic = "force-dynamic";
 
@@ -24,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <AuthProvider>
           <Layout>{children}</Layout>
         </AuthProvider>
